9b4b7a65c8d45337ef409882913ddb55388d2fb6
davorce <cepindavor@gmail.com>
2025-01-20 21:28:33 +0100

    Prvi update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         LocalDateTime dt = LocalDateTime.now();
-        System.out.println("Trenutno vrijem: " + dt);
+        System.out.println("Trenutno vrijeme: " + dt);
     }
 }
